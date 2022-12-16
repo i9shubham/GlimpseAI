cb7211afde50bde14de2381355636ed9f7326e7a
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2022-12-16 23:39:06 +0530

    adding environment variables

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ import express from 'express'
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { body, validationResult } from 'express-validator'
+import * as dotenv from 'dotenv'
+
+// import { body, validationResult } from 'express-validator'
+//body validaton will adding to next version
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = process.env.port | 3000;
+dotenv.config() // fetching environment variables
+const port = process.env.PORT;
 
 
-app.use(express.static('Public'))
+app.use(express.static('Public'));
+app.use(express.json()); //midlewares
+app.use(express.urlencoded());
 
 app.get("/", (req, res)=>{
     res.sendFile( __dirname + "/Public/views/index.html")
 })
 
-app.post("/")
+app.post("/getImage", (req, res ,next)=>{
+    // res.json(req.body);
+    console.log(req.body.prompt);
+})
 
 
 app.listen(port, ()=>{
