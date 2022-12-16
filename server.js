a857496a8a54f882948c8a9b5d06be3786defc42
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2022-12-17 01:42:20 +0530

    image fetching and openening done

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import * as dotenv from 'dotenv'
+import open from 'open'
+
+import { Configuration, OpenAIApi } from 'openai'
+import { url } from 'inspector';
 
 // import { body, validationResult } from 'express-validator'
 //body validaton will adding to next version
@@ -11,7 +15,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 dotenv.config() // fetching environment variables
 const port = process.env.PORT;
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
+
+
+  
 
 app.use(express.static('Public'));
 app.use(express.json()); //midlewares
@@ -21,9 +31,27 @@ app.get("/", (req, res)=>{
     res.sendFile( __dirname + "/Public/views/index.html")
 })
 
-app.post("/getImage", (req, res ,next)=>{
+app.post("/getImage", async(req, res ,next)=>{
     // res.json(req.body);
-    console.log(req.body.prompt);
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createImage({
+    prompt: req.body.prompt,
+    n: parseInt(req.body.n), //parseInt is a javascript function which converts string/json(string) into integer
+    // size: JSON.parse(req.body.size), //trying to parse size as a json but got error in multiply sign
+    size: req.body.size,
+    response_format: "url", //there is also a b64_json format
+    });
+    //working on opening multiple files/images
+    // const n = req.body.n;
+    // console.log(n);
+    // for(var i =0; i<n; i++){
+    //     var imgUrl = response.data.data[i].url; 
+    //     await open(imgUrl);
+    //     res.send(response.data.data[i].url);
+    // }
+    const imgUrl = response.data.data[0].url; 
+    await open(imgUrl, {app: {name: 'chrome'}});
+    res.send(response.data.data[0].url);
 })
 
 
