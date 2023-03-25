30848db04de701fbed88361b807d7fa1e292b144
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-03-25 09:46:46 +0530

    UI changed

--- index.js
+++ index.js
@@ -1,63 +1,68 @@
-import express from 'express'
-import path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import * as dotenv from 'dotenv'
-import { Configuration, OpenAIApi } from 'openai'
+import express from "express";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname + '/views/'))
-app.set('view engine', 'pug') //setting up a view engine
-app.use(express.static('Public'));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname + "/views/"));
+app.set("view engine", "pug"); //setting up a view engine
+app.use(express.static("Public"));
 app.use(express.json()); //midlewares
 app.use(express.urlencoded({ extended: true }));
-
-dotenv.config() // fetching environment variables
+app.use(express.static(__dirname));
+dotenv.config(); // fetching environment variables
 
 const port = process.env.PORT;
-const configuration = new Configuration({
+try {
+  var configuration = new Configuration({
+    // organization: org - II4yGG90SNoaZj4VRqhukhre,
     apiKey: process.env.OPENAI_API_KEY,
-});
+  });
+} catch (error) {
+  console.log(error);
+}
 
 const images = [
-    './teddy_shopping.jpg',
-    './ancestors_vr.jpg',
-    './astro_horse.jpg',
-    './teddybear_exp.jpg'
-]
+  "./teddy_shopping.jpg",
+  "./ancestors_vr.jpg",
+  "./astro_horse.jpg",
+  "./teddybear_exp.jpg",
+];
 
 app.get("/", (req, res) => {
-    // console.log(images)
-    res.render("index", {images});
+  // console.log(images)
+  res.render("index", { images });
 });
 
 app.post("/getImage", async (req, res, next) => {
-    try {
-        const openai = new OpenAIApi(configuration);
+  try {
+    const openai = new OpenAIApi(configuration);
     const response = await openai.createImage({
-        prompt: req.body.prompt,
-        n: +req.body.n,
-        size: req.body.size,
-        response_format: "url", //there is also a b64_json format
+      prompt: req.body.prompt,
+      n: +req.body.n,
+      size: req.body.size,
+      response_format: "url", //there is also a b64_json format
     });
-    const images = response.data.data.map(image => image.url);
+    const images = response.data.data.map((image) => image.url);
 
-    // console.log(images);
-    res.render("new", { images: images });
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-})
+    console.log(images);
+    res.render("index", { images: images });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-})
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
